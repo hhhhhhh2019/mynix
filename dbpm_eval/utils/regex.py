639911901931c78c397b19e4aead7ad038d19a12
Alex <agalove543210@gmail.com>
2024-06-02 21:07:25 +0300

    regex syntax parse

---
+++ dbpm_eval/utils/regex.py
@@ -0,0 +1,144 @@
+from dataclasses import dataclass
+from pprint import pprint
+
+
+@dataclass
+class Node:
+    min: int
+    max: int
+    paral: set
+    seq: list
+
+
+expression: list = []
+
+
+def parse_number():
+    global expression
+
+    s = ""
+
+    while len(expression) > 0 and expression[0] in "0123456789":
+        s += expression.pop(0)
+
+    return int(s)
+
+
+def parse_modifier():
+    global expression
+
+    match expression[0]:
+        case '+':
+            expression.pop(0)
+            return (1, -1)
+        case '*':
+            expression.pop(0)
+            return (0, -1)
+        case '?':
+            expression.pop(0)
+            return (0, 1)
+        case '{':
+            expression.pop(0)
+            n1 = parse_number()
+            c = expression.pop(0)
+            if c == '}':
+                return (n1, n1)
+            elif c != ',':
+                print("Error")
+            n2 = parse_number()
+            return (n1, n2)
+    return (0, 0)
+
+
+def parse_set():
+    global expression
+
+    node = Node(1, 1, set(), [])
+
+    while True:
+        if len(expression) == 0:
+            print("Error")
+            break
+
+        c = expression.pop(0)
+
+        if c == ']':
+            break
+
+        if c == "\\":
+            c = expression.pop(0)
+
+            if c == 'd':
+                node.paral += list("0123456789")
+            elif c == 's':
+                node.paral += list(" \n\t\r")
+
+        else:
+            node.paral.add(c)
+
+    return node
+
+
+def parse_group(must_br=True):
+    global expression
+
+    node = Node(1, 1, set(), [])
+
+    while True:
+        if len(expression) == 0:
+            if must_br:
+                print("Error")
+            break
+
+        c = expression.pop(0)
+        cnode = None
+
+        if c == ')':
+            if not must_br:
+                print("Error")
+            break
+
+        if c == '(':
+            cnode = parse_group()
+
+        elif c == '[':
+            cnode = parse_set()
+
+        elif c not in "+*?{":
+            if c == '\\':
+                c = expression.pop(0)
+
+                if c == 'd':
+                    cnode = Node(1, 1, set("0123456789"), [])
+                elif c == 's':
+                    cnode = Node(1, 1, set(" \n\t\r"), [])
+            else:
+                cnode = Node(1, 1, set(c), [])
+
+        if len(expression) > 0 and expression[0] in "+*?{":
+            if cnode is None:
+                print("Error")
+                break
+
+            cnode.min, cnode.max = parse_modifier()
+
+        if cnode is None:
+            print("Error")
+            break
+
+        node.seq.append(cnode)
+
+    return node
+
+
+def FA_from_regex(ex: str) -> list:
+    global expression
+
+    expression = list(ex)
+
+    node = parse_group(False)
+
+    pprint(node)
+
+
+FA_from_regex("(\\dabc)+he*ll[abc]{2,5}o")
