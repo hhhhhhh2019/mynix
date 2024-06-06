fc51833663d23b2a663a519eff46a5c0ca8a9198
Alex <agalove543210@gmail.com>
2024-06-06 10:56:00 +0300

    make regex.py again

--- dbpm_eval/utils/regex.py
+++ dbpm_eval/utils/regex.py
@@ -6,11 +6,10 @@ from pprint import pprint
 class Node:
     min: int
     max: int
-    paral: set
-    seq: list
+    data: set | list | str
 
     def __hash__(self):
-        return hash((self.min, self.max, tuple(self.paral), tuple(self.seq)))
+        return hash((self.min, self.max, tuple(self.data)))
 
 
 @dataclass
@@ -71,7 +70,7 @@ def parse_modifier():
 def parse_set() -> Node:
     global expression
 
-    node = Node(1, 1, set(), [])
+    result = Node(1, 1, set())
 
     while True:
         if len(expression) == 0:
@@ -83,27 +82,25 @@ def parse_set() -> Node:
         if c == ']':
             break
 
-        # if c == '(':
-        #     node.paral.add(parse_group())
-
-        if c == "\\":
+        if c == '(':
+            result.data.add(parse_group())
+        elif c == '\\':
             c = expression.pop(0)
 
             if c == 'd':
-                node.paral.update(set("0123456789"))
+                result.data.update(set("0123456789"))
             elif c == 's':
-                node.paral.update(set(" \n\t\r"))
-
+                result.data.update(set(" \n\r\t"))
         else:
-            node.paral.add(c)
+            result.data.add(c)
 
-    return node
+    return result
 
 
 def parse_group(must_br=True) -> Node:
     global expression
 
-    node = Node(1, 1, set(), [])
+    result = Node(1, 1, [])
 
     while True:
         if len(expression) == 0:
@@ -112,125 +109,31 @@ def parse_group(must_br=True) -> Node:
             break
 
         c = expression.pop(0)
-        cnode = None
 
         if c == ')':
             if not must_br:
                 print("Error")
             break
 
-        if c == '(':
-            cnode = parse_group()
+        if c == '[':
+            result.data.append(parse_set())
 
-        elif c == '[':
-            cnode = parse_set()
+        elif c == '\\':
+            c = expression.pop(0)
 
-        elif c not in "+*?{":
-            if c == '\\':
-                c = expression.pop(0)
+            if c == 'd':
+                result.data.append(Node(1, 1, set("0123456789")))
+            elif c == 's':
+                result.data.append(Node(1, 1, set(" \t\n\r")))
 
-                if c == 'd':
-                    cnode = Node(1, 1, set("0123456789"), [])
-                elif c == 's':
-                    cnode = Node(1, 1, set(" \n\t\r"), [])
-            else:
-                cnode = Node(1, 1, set(c), [])
+        else:
+            result.data.append(Node(1, 1, c))
 
-        if len(expression) > 0 and expression[0] in "+*?{":
-            if cnode is None:
-                print("Error")
-                break
+        if len(expression) == 0:
+            continue
 
-            cnode.min, cnode.max = parse_modifier()
-
-        if cnode is None:
-            print("Error")
-            break
-
-        node.seq.append(cnode)
-
-    return node
-
-
-def FANode_from_char(node: Node) -> FANode:
-    return FANode(node.paral, set())
-
-
-def FANode_from_node(node: Node) -> FANode | FAGroup:
-    if len(node.seq) == 0:
-        return FANode_from_char(node)
-    return FA_from_group(node)
-
-
-def FA_from_group(node: Node) -> FAGroup:
-    result = FAGroup([], [], [])
-
-    for i in node.seq:
-        count = max(1, i.min if i.max == -1 else i.max)
-        size = 1
-
-        for j in range(count):
-            fanode = FANode_from_node(i)
-
-            if isinstance(fanode, FAGroup):
-                if len(result.nodes) != 0:
-                    result.nodes[-1].next.update(
-                        set([len(result.nodes) + k for k in fanode.inputs]))
-                for m in fanode.nodes:
-                    m.next = set([len(result.nodes) + k for k in m.next])
-
-                if i.min <= j:
-                    for m in fanode.nodes:
-                        m.can_zero = True
-
-                result.nodes += fanode.nodes
-
-                size = len(fanode.nodes)
-
-            else:
-                fanode.next.add(len(result.nodes) + 1)
-
-                if i.min <= j:
-                    fanode.can_zero = True
-
-                result.nodes.append(fanode)
-
-        if i.max == -1:
-            result.nodes[-1].next.add(len(result.nodes) - size)
-
-    for i in range(len(result.nodes)-2, -1, -1):
-        j = 0
-        next_list = list(result.nodes[i].next)
-
-        while j < len(result.nodes[i].next):
-            id = next_list[j]
-
-            if id == len(result.nodes):
-                j += 1
-                continue
-
-            if not result.nodes[id].can_zero:
-                j += 1
-                continue
-
-            if result.nodes[id].next.issubset(result.nodes[i].next):
-                j += 1
-                continue
-
-            result.nodes[i].next.update(result.nodes[id].next)
-            next_list = list(result.nodes[i].next)
-
-            j = 0
-
-    for id, i in enumerate(result.nodes):
-        result.inputs.append(id)
-
-        if not i.can_zero:
-            break
-
-    for id, i in enumerate(result.nodes):
-        if len(result.nodes) in i.next:
-            result.outputs.append(id)
+        if expression[0] in "+*?{":
+            result.data[-1].min, result.data[-1].max = parse_modifier()
 
     return result
 
@@ -244,9 +147,6 @@ def FA_from_regex(ex: str) -> list:
 
     pprint(node)
 
-    group = FA_from_group(node)
-    pprint(group)
 
-
-FA_from_regex("(abc(123\\d){1,3})+")
-# FA_from_regex(r"abc(123){2,5}")
+FA_from_regex(r"[(a?b)(b*c)123]")
+# FA_from_regex(r"[+-]?([123456789]\d*[eE][123456789]\d*|(([123456789]\d*\.)|(\.\d+))(\d*)?([eE][\-\+]?[123456789]\d*)?)")
